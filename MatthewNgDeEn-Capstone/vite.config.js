838021b09d998958aada6f5330a808c26d7e4a7a
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-03-04 20:36:31 +0800

    revert

--- MatthewNgDeEn-Capstone/vite.config.js
+++ MatthewNgDeEn-Capstone/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/MatthewNgDeEn-Capstone/', // Base URL should be your repository name
+  // base: '/MatthewNgDeEn-Capstone/', // Base URL should be your repository name
 })
 
 
