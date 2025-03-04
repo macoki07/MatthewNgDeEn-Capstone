a53a1cf3228b61e876b5206c1fb0e1c0dec20896
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-03-04 20:21:28 +0800

    Added workflow and modified vite config

--- MatthewNgDeEn-Capstone/vite.config.js
+++ MatthewNgDeEn-Capstone/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/MatthewNgDeEn-Capstone/', // Base URL should be your repository name
+  root: 'MatthewNgDeEn-Capstone' // Set the root directory for Vite
 })
+
+
+
